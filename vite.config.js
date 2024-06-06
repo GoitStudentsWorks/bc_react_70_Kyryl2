eef0ac5d2546090215d10d0916eed08f866f1c10
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-06-06 23:25:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bc_react_70_Kyryl2/"
+  base: "/bc_react_70_Kyryl2/",
   build: {
     sourcemap: true,
   },
